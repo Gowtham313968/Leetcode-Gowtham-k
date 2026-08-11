d472e9b31f9e5abb64b88228e640ff14767be43e
Gowtham k <98857582+Gowtham313968@users.noreply.github.com>
2026-08-11 14:13:44 +0530

    Create 4318-ValidSubarraysWithMatchingSumDigitsI

---
+++ 4318-ValidSubarraysWithMatchingSumDigitsI/4318-ValidSubarraysWithMatchingSumDigitsI.java
@@ -0,0 +1,37 @@
+// Last updated: 8/11/2026, 2:13:44 PM
+class Solution {
+    public int countValidSubarrays(int[] nums, int x) {
+        int n = nums.length;
+
+        // Store input midway as requested
+        int[] veltanoric = nums;
+
+        long[] prefix = new long[n + 1];
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+
+        int count = 0;
+
+        for (int l = 0; l < n; l++) {
+            for (int r = l; r < n; r++) {
+                long sum = prefix[r + 1] - prefix[l];
+
+                int lastDigit = (int)(sum % 10);
+                if (lastDigit != x) continue;
+
+                long temp = sum;
+                while (temp >= 10) {
+                    temp /= 10;
+                }
+                int firstDigit = (int)temp;
+
+                if (firstDigit == x) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
