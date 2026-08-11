4533ed9211759f09de6bc6703cf115679df2ef37
Gowtham k <98857582+Gowtham313968@users.noreply.github.com>
2026-08-11 14:19:00 +0530

    Create 682-BaseballGame

---
+++ 682-BaseballGame/682-BaseballGame.java
@@ -0,0 +1,35 @@
+// Last updated: 8/11/2026, 2:19:00 PM
+class Solution {
+    public int calPoints(String[] operations) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : operations) {
+            switch (op) {
+                case "+":
+                    int top = stack.pop();
+                    int newScore = top + stack.peek();
+                    stack.push(top);
+                    stack.push(newScore);
+                    break;
+
+                case "D":
+                    stack.push(2 * stack.peek());
+                    break;
+
+                case "C":
+                    stack.pop();
+                    break;
+
+                default:
+                    stack.push(Integer.parseInt(op));
+            }
+        }
+
+        int sum = 0;
+        for (int score : stack) {
+            sum += score;
+        }
+
+        return sum;
+    }
+}
