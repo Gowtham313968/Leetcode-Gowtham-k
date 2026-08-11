a27f6369eca8dd28dbff15bd08e3277b570a08b7
Gowtham k <98857582+Gowtham313968@users.noreply.github.com>
2026-08-11 14:17:19 +0530

    Create 1135-CustomersWhoBoughtAllProducts

---
+++ 1135-CustomersWhoBoughtAllProducts/1135-CustomersWhoBoughtAllProducts.sql
@@ -0,0 +1,9 @@
+-- Last updated: 8/11/2026, 2:17:19 PM
+SELECT
+    customer_id
+FROM Customer
+GROUP BY customer_id
+HAVING COUNT(DISTINCT product_key) = (
+    SELECT COUNT(*)
+    FROM Product
+);
