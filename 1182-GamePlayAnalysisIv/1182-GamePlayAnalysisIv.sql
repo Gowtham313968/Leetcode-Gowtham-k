95617074f525061592d12a8f8eb6cdc661cf1bc0
Gowtham k <98857582+Gowtham313968@users.noreply.github.com>
2026-08-11 14:16:58 +0530

    Create 1182-GamePlayAnalysisIv

---
+++ 1182-GamePlayAnalysisIv/1182-GamePlayAnalysisIv.sql
@@ -0,0 +1,16 @@
+-- Last updated: 8/11/2026, 2:16:58 PM
+SELECT
+    ROUND(
+        COUNT(DISTINCT a.player_id) /
+        (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM Activity a
+JOIN Activity b
+ON a.player_id = b.player_id
+AND b.event_date = DATE_ADD(a.event_date, INTERVAL 1 DAY)
+WHERE (a.player_id, a.event_date) IN (
+    SELECT player_id, MIN(event_date)
+    FROM Activity
+    GROUP BY player_id
+);
