6e2c13196935af472f5189ac8b070406890d0ec4
Gowtham k <98857582+Gowtham313968@users.noreply.github.com>
2026-08-11 14:16:08 +0530

    Create 1454-RemovePalindromicSubsequences

---
+++ 1454-RemovePalindromicSubsequences/1454-RemovePalindromicSubsequences.java
@@ -0,0 +1,20 @@
+// Last updated: 8/11/2026, 2:16:08 PM
+class Solution {
+    public int removePalindromeSub(String s) {
+        if (s.length() == 0)
+            return 0;
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right))
+                return 2;
+
+            left++;
+            right--;
+        }
+
+        return 1;
+    }
+}
